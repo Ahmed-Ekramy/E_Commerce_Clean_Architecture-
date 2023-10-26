6dbb4d822d10800a6ec01bef964df8ec470a52d6
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2023-10-26 14:11:13 -0700

    home state

---
+++ lib/features/home_layout/presentation/manager/home_state.dart
@@ -0,0 +1,12 @@
+
+abstract class HomeState {}
+class HomeInitial extends HomeState {}
+class CatLoadingState extends HomeState {}
+class CatSuccessState extends HomeState {}
+class CatFailureState extends HomeState {}
+///////////////////////////////////////////////////////
+//cat//
+//////////////////////////////////////////////////////
+class BrandLoadingState extends HomeState {}
+class BrandSuccessState extends HomeState {}
+class BrandFailureState extends HomeState {}
